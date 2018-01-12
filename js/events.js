179860a7944c1ad5271b64fa8da0d656920fded3
Chen Ye <dpp0921@gmail.com>
2018-01-12 20:10:03 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -9,6 +9,13 @@ function frameIt(){
     $('img').addClass('tasty')
   })
 }
+function pressIt(){
+  $('#typing').on('keydown', function(e){
+    if(e.which === 71){
+      alert(`Your form is going to be submitted now.`)
+    }
+  })
+}
 $(document).ready(function(){
 
 // call functions here
